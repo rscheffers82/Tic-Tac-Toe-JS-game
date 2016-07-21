2a8ff1048be804f9d5aee7cc96d8a21a73356e9c
Roy Scheffers <r.scheffers@gmail.com>
2016-07-21 11:10:03 -0400

    Add app structure and initial functions

--- main_script.js
+++ main_script.js
@@ -2,3 +2,16 @@
 $( document ).ready(function() {
 
 });
+
+var gameState = {}		// 1, 2, 3
+						// 4, 5, 6
+						// 7, 8, 9
+
+var clearBoard = function(){
+
+}
+
+
+$('.tile').on('click', function(){
+	console.log(this);
+});
