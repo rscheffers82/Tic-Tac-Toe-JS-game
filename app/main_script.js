3fdf30993c4f56cfc7768c57a12ab732090d0887
Roy Scheffers <r.scheffers@gmail.com>
2016-07-31 12:02:47 -0400

    Separate overlay box from main_script.js into overlay.js
    Add settings functionality

--- app/main_script.js
+++ app/main_script.js
@@ -10,10 +10,10 @@ var game = (function(){
 				 0,0,0];
 	var AI;					// true computer / false 2 players
 	var player = [];		// Object: Name, Icon, Win, Draw
-	var difficulty = 95;	// 0 to 100, 100: player never wins
-	var draw = 0;
+	var difficulty;			// 0 to 100, 100: player never wins
+	var draw;
 	var turn;				// which player is allowed to make a move
-	var start;				// player 0 starts the game
+	var start = 0;				// player 0 starts the game
 	// private functions
 	var reset = function(){
 		//console.log('reset game');
@@ -79,14 +79,14 @@ var game = (function(){
 		},
 		init: function(){
 			loadBoard();
-			game.loadPlayers();
+			//game.loadGamePlay();
 			start = 1;			// needed as reset switches players 
 			reset();
 		},
-		loadPlayers: function(){
+		setGameValues: function(){
 			AI = $('.pl-selected').attr('id');
-			player[0] = {}; player[1] = {};
 			difficulty = $('#difficulty').val();
+			player[0] = {}; player[1] = {};
 
 			// load player names 
 			player[0].name = $('#player1').val() === '' ? 'Player1' : $('#player1').val();
@@ -104,21 +104,12 @@ var game = (function(){
 				player[1].icon = 'o';
 			}
 
-	console.log(AI);
-	console.log(player);
-/*
-			player[0] = {
-				name: 'Tic Tac Toe King Royko',
-				icon: 'x',
-				win: 0
-			}
-			player[1] = {
-			//name: 'IA (' + difficulty + ')',
-				name: 'Zorana Queen Quick-thinker',
-				icon: 'o',
-				win: 0
-			}
-*/
+			player[0].win = 0;
+			player[1].win = 0;
+			draw = 0;
+
+			console.log(AI);
+			console.log(player);
 		},
 		fill: function(tile){
 			var pos = tile.currentTarget.id;
@@ -135,97 +126,17 @@ $('body').on('click', '.tile', function(event){
 	game.fill(event);
 });
 
-
-var loadSingle = function(){
-    $('.overlaybox').empty();
-    
-    var title = '<div class="overlaybox-caption">' +
-    		'<span class="pl pl-selected" id="true">Single-player</span> | <span class="pl" id="false">Multi-player</span>' +
-    	'</div>';
-    
-    var content = '<div class="overlaybox-content">' +
-    	'<p>Player: <i class="note">(optional)</i> <input type="text" id="player1"></p>' +
-        '<p>' +
-            '<img class="icon icon-selected" id="x" src="images/x.png">' +
-            '<img class="icon" id="o" src="images/o.png">' +
-        '</p>' +
-        '<p class="note">* x starts the first game</p>' +
-        '<p>Difficulty: ' +
-            '<select id="difficulty">' +
-                '<option value="75">HARD:   Face a butt-kicking AI</option>' +
-                '<option value="50">MEDIUM: AI lets you win once in a while ;)</option>' +
-                '<option value="25">EASY:   If you are lacking courage</option>' +
-            '</select>' +
-        '</p>' +
-        '<button type="button" onclick="start()">Let\'s get started...</button>' + 
-        '</div>';
-    
-    $('.overlaybox').append(title + content);
-}
-
-var loadMulti = function(){
-    $('.overlaybox').empty();
-    
-    var title = '<div class="overlaybox-caption">' +
-    		'<span class="pl" id="true">Single-player</span> | <span class="pl pl-selected" id="false">Multi-player</span>' +
-    	'</div>';
-    
-    var content = '<div class="overlaybox-content">' +
-	    '<div class="tab">' +
-    	    '<div class="tr">' +
-        	    '<div class="td"><img class="icon" id="x" src="images/x.png"></div>' +
-            	'<div class="td">' +
-                    '<div>Player 1: </div><input type="text" id="player1">' +
-                   	'<div class="note less-margin">* x starts the first game</div>' +
-	            '</div>' +
-    	    '</div>' +
-
-        	'<div class="tr">' +
-            	'<div class="td"><img class="icon" id="o" src="images/o.png"></div>' +
-	            '<div class="td">' +
-    	            '<div>Player 2: </div><input type="text" id="player2">' + 
-    	        '</div>' +
-        	'</div>' +
-        '</div>' +
-        '<button type="button" onclick="start()">Let\'s get started...</button>' +
-    '</div>';
-
-    $('.overlaybox').append(title + content);
-}
-var showScore = function(){
-    $('.overlaybox').empty();
-
-    var title = '<div class="overlaybox-caption">Score</div>';
-    var content = '<div class="overlaybox-content">' +
-    	'<div class="summary">' +
-        	'Player 1 wins...' +
-	        '<div class="tab small">' +
-    	       '<div class="result-players tr">' +
-        	        '<div class="td left">Roy Scheffers:</div><div class="td right">7</div>' +
-            	'</div>' +
-	            '<div class="result-players tr">' +
-    	            '<div class="td left">IA (easy)</div><div class="td right">2</div>' +
-        	    '</div>' +
-            	'<div class="result-players tr">' +
-	                '<div class="td left">Draw:</div><div class="td right">15</div>' +
-    	        '</div>' +
-        	'</div>' +
-	    '</div>' +
-    	'<button type="button" onclick="start()">Play again...</button>' +
-    	'<button type="button" onclick="start()">Exit</button>' +
-    '</div>';
-    $('.overlaybox').append(title + content);
-
-    $('#myBox').css('height', '100%');
-}
-
 function showBox() {
     loadSingle();
-    //loadMulti();
-    //showScore();
     $('#myBox').css('height', '100%');
 }
 
+function start() {
+    game.setGameValues();
+    $('#myBox').css('height', '0%');
+}
+
+/*
 var grabSettings = function() {
 	var AI = $('.pl-selected').attr('id');
 	var player = []; player[0] = {}; player[1] = {};
@@ -250,21 +161,4 @@ var grabSettings = function() {
 	console.log(AI);
 	console.log(player);
 }
-
-function start() {
-    $('#myBox').css('height', '0%');
-    loadPlayers();
-}
-
-$('.overlaybox').on('click', '.pl', function(e){
-    if (this.id === 'true') loadSingle();
-    else if (this.id === 'false' ) loadMulti();
-});
-
-$('.overlaybox').on('click', '.icon' ,function(e){
-    console.log(e);
-
-    var cName = e.currentTarget.className;
-    $('.' + cName).removeClass(cName + '-selected');
-    $('#' + this.id).addClass(cName + '-selected');
-});
+*/
---
+++ app/overlay.js
@@ -0,0 +1,96 @@
+$('.overlaybox').on('click', '.pl', function(e){
+    // Single / Multi-player selection
+    if (this.id === 'true') loadSingle();               // AI mode, true
+    else if (this.id === 'false' ) loadMulti();         // Multi-player mode
+});
+
+$('.overlaybox').on('click', '.icon' ,function(e){
+    // icon change function
+    var cName = e.currentTarget.className;
+    $('.' + cName).removeClass(cName + '-selected');
+    $('#' + this.id).addClass(cName + '-selected');
+});
+
+
+var loadSingle = function(){
+    $('.overlaybox').empty();
+    
+    var title = '<div class="overlaybox-caption">' +
+    		'<span class="pl pl-selected" id="true">Single-player</span> | <span class="pl" id="false">Multi-player</span>' +
+    	'</div>';
+    
+    var content = '<div class="overlaybox-content">' +
+    	'<p>Player: <i class="note">(optional)</i> <input type="text" id="player1"></p>' +
+        '<p>' +
+            '<img class="icon icon-selected" id="x" src="images/x.png">' +
+            '<img class="icon" id="o" src="images/o.png">' +
+        '</p>' +
+        '<p class="note">* x starts the first game</p>' +
+        '<p>Difficulty: ' +
+            '<select id="difficulty">' +
+                '<option value="75">HARD:   Face a butt-kicking AI</option>' +
+                '<option value="50">MEDIUM: AI lets you win once in a while ;)</option>' +
+                '<option value="25">EASY:   If you are lacking courage</option>' +
+            '</select>' +
+        '</p>' +
+        '<button type="button" onclick="start()">Let\'s get started...</button>' + 
+        '</div>';
+    
+    $('.overlaybox').append(title + content);
+}
+
+var loadMulti = function(){
+    $('.overlaybox').empty();
+    
+    var title = '<div class="overlaybox-caption">' +
+    		'<span class="pl" id="true">Single-player</span> | <span class="pl pl-selected" id="false">Multi-player</span>' +
+    	'</div>';
+    
+    var content = '<div class="overlaybox-content">' +
+	    '<div class="tab">' +
+    	    '<div class="tr">' +
+        	    '<div class="td"><img class="icon" id="x" src="images/x.png"></div>' +
+            	'<div class="td">' +
+                    '<div>Player 1: </div><input type="text" id="player1">' +
+                   	'<div class="note less-margin">* x starts the first game</div>' +
+	            '</div>' +
+    	    '</div>' +
+
+        	'<div class="tr">' +
+            	'<div class="td"><img class="icon" id="o" src="images/o.png"></div>' +
+	            '<div class="td">' +
+    	            '<div>Player 2: </div><input type="text" id="player2">' + 
+    	        '</div>' +
+        	'</div>' +
+        '</div>' +
+        '<button type="button" onclick="start()">Let\'s get started...</button>' +
+    '</div>';
+
+    $('.overlaybox').append(title + content);
+}
+var showScore = function(){
+    $('.overlaybox').empty();
+
+    var title = '<div class="overlaybox-caption">Score</div>';
+    var content = '<div class="overlaybox-content">' +
+    	'<div class="summary">' +
+        	'Player 1 wins...' +
+	        '<div class="tab small">' +
+    	       '<div class="result-players tr">' +
+        	        '<div class="td left">Roy Scheffers:</div><div class="td right">7</div>' +
+            	'</div>' +
+	            '<div class="result-players tr">' +
+    	            '<div class="td left">IA (easy)</div><div class="td right">2</div>' +
+        	    '</div>' +
+            	'<div class="result-players tr">' +
+	                '<div class="td left">Draw:</div><div class="td right">15</div>' +
+    	        '</div>' +
+        	'</div>' +
+	    '</div>' +
+    	'<button type="button" onclick="start()">Play again...</button>' +
+    	'<button type="button" onclick="start()">Exit</button>' +
+    '</div>';
+    $('.overlaybox').append(title + content);
+
+    $('#myBox').css('height', '100%');
+}
